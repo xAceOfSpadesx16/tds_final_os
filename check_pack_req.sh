162482f18d43584581984faa85a48657ca515e8b
AoS <aos.fontana.dev@gmail.com>
2024-11-15 20:30:00 -0300

    chequeo de paquetes
    se definio script de chequeo de paquetes requeridos

---
+++ check_pack_req.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+source trebol.conf
+
+# Definicion de Array para paquetes faltantes
+missing_packages=()
+
+# Comprobacion de paquetes
+for package in "${req_packages[@]}"; do
+    if ! dpkg -l | grep -q "^ii  $package "; then
+        missing_packages+=("$package")
+    fi
+done
+
+# En caso de que todos los paquetes esten instalados finaliza el script correctamente
+if [ ${#missing_packages[@]} -eq 0 ]; then
+    echo "Todos los paquetes ya están instalados."
+    exit 0
+fi
+
+# Mostrar los paquetes faltantes
+echo "Los siguientes paquetes no están instalados:"
+printf '%s\n' "${missing_packages[@]}"
+echo
+read -p "¿Desea instalarlos? (s/n): " -n 1 -r respuesta
+
+# Validar la respuesta con un bucle while
+while [[ "$respuesta" != "s" && "$respuesta" != "n" ]]; do
+    echo "Entrada no válida."
+    read -p "Por favor, ingrese 's' para sí o 'n' para no." -n 1 -r respuesta
+done
+
+# Actuar según la respuesta
+if [[ "$respuesta" == "s" ]]; then
+    echo "Instalando paquetes..."
+    echo
+    sudo apt install -y "${missing_packages[@]}"
+
+    if [ $? -ne 0 ]; then
+        echo "Hubo un error al instalar los paquetes."
+        echo "Chequee el log de errores."
+        echo "Ejecucion cancelada, corrija los errores e intente nuevamente."
+        exit 1
+    fi
+else
+    echo "No se instalaran los paquetes."
+    echo "Ejecucion cancelada, los paquetes son requeridos para continuar."
+    exit 1
+fi
--- init.sh
+++ init.sh
@@ -1,28 +1,56 @@
 #!/bin/bash
 
+source trebol.conf
+
 # Check de Configuración de Disco a particionar
 sudo bash check_disk_config.sh
 if [ $? -ne 0 ]; then
     exit 1
 fi
 
+# Check de paquetes requeridos
+sudo bash check_pack_req.sh
+if [ $? -ne 0 ]; then
+    exit 1
+fi
+
 # Particionado de disco
+echo "Iniciando particionado de disco $DISCO."
+
 sudo bash particiones/main.sh
 
+echo -e "Particionado de disco $DISCO ha finalizado.\n"
+
 # Creación de estructura de directorios, definicion de archivos en skel, listas de usuarios.
+echo "Creando estructura de directorios y copiado de archivos necesarios."
+
 sudo bash definicion/main.sh
 
+echo -e "Creacion de estructura de directorios finalizada.\n"
+
 # Creacion de grupos definidos en problematica trebol (acorde a archivos .list)
+echo "Definiendo grupos."
+
 sudo bash definicion/groups.sh
+
+echo -e "Definicion de Grupos finalizada.\n"
 
 # Definicion de nuevo alias para creacion usuarios.
 
 # Creacion de usuarios definidos en problematica trebol
+echo "Creando usuarios de $empresa"
+
 sudo bash users/auto.sh
 
+echo -e "Creacion de Usuarios finalizada.\n"
+
 # Definicion de permisos de arbol y propietarios de directorios
+echo "Definiendo propietarios y permisos."
+
 sudo bash definicion/own_perms.sh
 
-#
+echo -e "Definicion de propietarios y permisos finalizada.\n"
+
+# Definiendo Active Directory
 
 #
